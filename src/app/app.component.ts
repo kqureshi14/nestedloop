91bc0b11e5ef3a717588df23fc4ae6abb94c6e82
Kaleem <kaleem14you@gmail.com>
2024-07-23 16:35:39 +0500

    NestedLoopAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'nestedloop';
+
+  users = [
+    {
+    name:'Kaleem', phone:'343534334', socialAccounts:['facebook','insta', 'gmail']
+  },
+  {
+    name:'Sami', phone:'354545454',socialAccounts:['Twitter','yahoo', 'TickTok']
+  },
+  {
+    name:'Smith', phone:'1645454545',socialAccounts:['facebook','Twitter', 'yahoo']
+  },
+  {
+    name:'John', phone:'17776756565',socialAccounts:['TickTok','insta', 'LinkedIn']
+  }
+]
 }
